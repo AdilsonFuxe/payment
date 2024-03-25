d2e4e76dbf612ec78844ea24245df1483f0ba726
Adilson Fuxe <adilsonfuxe.fuxe@gmail.com>
2024-03-25 03:06:06 +0100

    feat: ensure DbSignup calls addUserRepository with correct params

--- src/test/java/com/adilsonfuxe/payment/core/data/usecases/DbSignUpTest.java
+++ src/test/java/com/adilsonfuxe/payment/core/data/usecases/DbSignUpTest.java
@@ -39,4 +39,27 @@ public class DbSignUpTest {
     dbSignUp.signUp(params);
     Mockito.verify(passwordHash).hashPassword(params.getPassword());
   }
+
+  @Test
+  @DisplayName("Should call addUserRepository with correct params")
+  void testCallAddUserRepositoryWithCorrectParams(){
+    var params = SignUpParams.builder()
+        .firstName("any_first_name")
+        .lastName("any_last_name")
+        .email("any_email@mail.com")
+        .password("any_password")
+        .phone("any_phone")
+        .build();
+    String hashedPassword = "any_hashed_password";
+    Mockito.when(passwordHash.hashPassword(params.getPassword())).thenReturn(hashedPassword);
+    dbSignUp.signUp(params);
+    var serializedSignupParams = SignUpParams.builder()
+        .firstName("any_first_name")
+        .lastName("any_last_name")
+        .email("any_email@mail.com")
+        .password(hashedPassword)
+        .phone("any_phone")
+        .build();
+    Mockito.verify(addUserRepository).AddUser(serializedSignupParams);
+  }
 }
